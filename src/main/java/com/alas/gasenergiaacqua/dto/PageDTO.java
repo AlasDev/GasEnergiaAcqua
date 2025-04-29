118cf481c141220fa002d7558a1728ea167cbaa5
AlasDev <saladavide003@gmail.com>
2025-04-29 10:46:14 +0200

    Add Page DTO

---
+++ src/main/java/com/alas/gasenergiaacqua/dto/PageDTO.java
@@ -0,0 +1,62 @@
+package com.alas.gasenergiaacqua.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * DTO to display data as a page
+ * @param <DTO> the dtos you want to show inside the page
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageDTO<DTO> {
+    /**
+     * Il contenuto della pagina
+     */
+    @NotNull
+    private List<DTO> content;
+
+    /**
+     * Numero di elementi massimi che la pagina può contenere
+     */
+    @NotNull
+    private Integer size;
+
+    /**
+     * Numero di elementi effettivamente presenti nella pagina
+     */
+    @NotNull
+    private Integer numberOfElements;
+
+    /**
+     * True quando sei alla pagina iniziale (pagina 0)
+     */
+    @NotNull
+    private Boolean first;
+
+    /**
+     * True quando sei all'ultima pagina
+     */
+    @NotNull
+    private Boolean last;
+
+    /**
+     * Numero della pagina (parte da 0)
+     */
+    @NotNull
+    private Integer number;
+
+    /**
+     * Numero di pagine
+     */
+    @NotNull
+    private Integer totalPages;
+
+}
