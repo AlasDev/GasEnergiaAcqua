31fb54e3e7c9fc78c204eb2dc1cf499ef6148f44
AlasDev <saladavide003@gmail.com>
2025-05-06 10:04:19 +0200

    Add Bcrypt encryption class

---
+++ src/main/java/com/alas/gasenergiaacqua/util/PasswordUtil.java
@@ -0,0 +1,25 @@
+package com.alas.gasenergiaacqua.util;
+
+import org.springframework.context.annotation.Configuration;
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@Configuration
+public class PasswordUtil {
+    /**
+     * @param password the raw password you want to hash
+     * @return the hashed password
+     */
+    public String hash(String password) {
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    }
+
+    /**
+     * @param password the raw password
+     * @param hashed the hashed password you want to campare the raw password with
+     * @return true if they match, false otherwise
+     */
+    public boolean verify(String password, String hashed) {
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hashed);
+        return result.verified;
+    }
+}
