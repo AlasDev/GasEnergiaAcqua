b65c7efb22a3eb6c8e34e7a585351d546f58b20c
AlasDev <saladavide003@gmail.com>
2025-05-06 17:01:55 +0200

    Add/Edit utilityMeter DTOs

--- src/main/java/com/alas/gasenergiaacqua/dto/UtilityMeterNewDTO.java
+++ src/main/java/com/alas/gasenergiaacqua/dto/UtilityMeterNewDTO.java
@@ -5,16 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UtilityMeterDTO {
-    private UUID id;
+public class UtilityMeterNewDTO {
     private UUID userId;
     private UUID addressId;
 
@@ -25,9 +22,4 @@ public class UtilityMeterDTO {
     private String serialNumber;
     private String meterName;
     private Boolean isActive;
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    private List<UUID> readingsIds;
 }
---
+++ src/main/java/com/alas/gasenergiaacqua/dto/UtilityMeterSummaryDTO.java
@@ -0,0 +1,19 @@
+package com.alas.gasenergiaacqua.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UtilityMeterSummaryDTO {
+    private UUID id;
+    private Integer resourceTypeId;
+    private String serialNumber;
+    private String meterName;
+}
