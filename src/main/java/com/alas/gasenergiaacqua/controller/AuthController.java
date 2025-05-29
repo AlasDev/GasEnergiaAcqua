ffb04caba3ff472f832b78921959efd075e84671
AlasDev <saladavide003@gmail.com>
2025-05-29 15:32:59 +0200

    Edit Added new endpoint for token validation

--- src/main/java/com/alas/gasenergiaacqua/controller/AuthController.java
+++ src/main/java/com/alas/gasenergiaacqua/controller/AuthController.java
@@ -1,23 +1,25 @@
 package com.alas.gasenergiaacqua.controller;
 
+import com.alas.gasenergiaacqua.auth.JwtTokenProvider;
 import com.alas.gasenergiaacqua.dto.ResponseMessage;
 import com.alas.gasenergiaacqua.dto.UserLoginDTO;
 import com.alas.gasenergiaacqua.dto.UserRegisterDTO;
+import com.alas.gasenergiaacqua.exception.AuthenticationException;
 import com.alas.gasenergiaacqua.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, JwtTokenProvider jwtTokenProvider) {
         this.authService = authService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     /**
@@ -36,5 +38,13 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseMessage register(@Validated @RequestBody UserRegisterDTO registerCredentials) {
         return authService.register(registerCredentials);
+    }
+
+    @GetMapping("/tokenValidation")
+    public boolean validateToken(@RequestHeader("Authorization") String jwtToken) {
+        if (jwtTokenProvider.isTokenEmpty(jwtToken) || !jwtTokenProvider.isValid(jwtToken) || jwtTokenProvider.isTokenExpired(jwtToken)) {
+            return false;
+        }
+        return true;
     }
 }
