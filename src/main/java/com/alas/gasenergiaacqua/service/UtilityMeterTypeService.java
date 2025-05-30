b4912c00faa9a2c2220a57643ec548da75a4824b
AlasDev <saladavide003@gmail.com>
2025-05-30 12:52:44 +0200

    Add Added service for UtilityMeterType

--- src/main/java/com/alas/gasenergiaacqua/service/UtilityMeterTypeService.java
+++ src/main/java/com/alas/gasenergiaacqua/service/UtilityMeterTypeService.java
@@ -1,10 +1,37 @@
 package com.alas.gasenergiaacqua.service;
 
+import com.alas.gasenergiaacqua.dto.UtilityMeterTypeDTO;
+import com.alas.gasenergiaacqua.mapper.UtilityMeterTypeMapper;
 import com.alas.gasenergiaacqua.repository.UtilityMeterTypeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
 public class UtilityMeterTypeService {
-    public UtilityMeterTypeService(UtilityMeterTypeRepository utilityMeterTypeRepository) {
+
+    private final UtilityMeterTypeRepository utilityMeterTypeRepository;
+    private final UtilityMeterTypeMapper utilityMeterTypeMapper;
+
+    public UtilityMeterTypeService(UtilityMeterTypeRepository utilityMeterTypeRepository, UtilityMeterTypeMapper utilityMeterTypeMapper) {
+        this.utilityMeterTypeRepository = utilityMeterTypeRepository;
+        this.utilityMeterTypeMapper = utilityMeterTypeMapper;
+    }
+
+    /**
+     * @param id uuid of the Utility Meter Type you want to get
+     * @return a DTO if found
+     */
+    public UtilityMeterTypeDTO getById(Integer id) {
+        return utilityMeterTypeMapper.mapToDto(utilityMeterTypeRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Cannot find!\nNo utility meter type was found with id: " + id)));
+    }
+
+    /**
+     * @return a list of DTO
+     */
+    public List<UtilityMeterTypeDTO> getAll() {
+        return utilityMeterTypeMapper.mapToDtos(utilityMeterTypeRepository.findAll());
     }
 }
